77968b365958057c761590b39a4a5be635320cbd
CharlesES <elsysoavitac@gmail.com>
2023-02-03 23:29:55 +0300

    init modif

--- src/main/java/com/example/enchere/repository/DemanderechargeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.enchere.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.enchere.model.Demanderecharge;
-
-public interface DemanderechargeRepository  extends JpaRepository<Demanderecharge,Long>{
-    
-}
