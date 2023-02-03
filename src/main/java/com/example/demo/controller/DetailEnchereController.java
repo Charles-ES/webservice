6e02b03c5252620ee4d67c080f2180e2bf010c24
CharlesES <elsysoavitac@gmail.com>
2023-02-03 23:42:01 +0300

    update

--- src/main/java/com/example/demo/controller/DetailEnchereController.java
+++ src/main/java/com/example/demo/controller/DetailEnchereController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -15,6 +16,7 @@ import com.example.demo.repository.DetailEnchereRepository;
 import com.example.demo.response.Succes;
 
 @Controller
+@CrossOrigin("*")
 public class DetailEnchereController {
     @Autowired
     DetailEnchereRepository detailRepository;
